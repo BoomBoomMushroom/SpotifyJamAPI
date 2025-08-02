27e7efff7d8370a9a03db825962d95bb65778642
BoomBoomMushroom <dillioncat7@gmail.com>
2025-08-02 17:39:55 -0400

    Made the API!

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    longDescription = f.read()
+
+setup(
+    name='spotify-jam',
+    description='spotify-jam is a package that allows you to manage Spotify Jams',
+    long_description=longDescription,
+    long_description_content_type="text/markdown",
+    version='0.1.0',
+    packages=find_packages(),
+    install_requirements=[
+        'requests==2.32.4'
+    ],
+)
